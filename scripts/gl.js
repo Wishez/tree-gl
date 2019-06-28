31097d2cbfdebae1bed0945a7c05798c0afe71f1
Filipp Zhuravlev <shiningfinger@list.ru>
2019-06-28 20:21:39 +0300

    Split code. Make 3d matrix.

--- scripts/gl.js
+++ scripts/gl.js
@@ -14,83 +14,8 @@ window.addEventListener('load', () => {
     .catch(error => console.error(error))
 })
 
-const m3 = (function() {
-  const FIRST = 'first'
-  const SECOND = 'second'
-  const THIRD = 'third'
-  const that = { 
-    projection: (width, height) => ([
-      2 / width, 0, 0,
-      0, -2 / height, 0,
-      -1, 1, 1,
-    ]),
 
-    identity: () => ([
-      1, 0, 0,
-      0, 1, 0,
-      0, 0, 1,
-    ]),
-
-    rotation: (angleInRadians) => {
-      const s = Math.sin(angleInRadians)
-      const c = Math.cos(angleInRadians)
-      return [
-        c, -s, 0,
-        s, c, 0,
-        0, 0, 1,
-      ]
-    },
-
-    transition: (x, y) => ([
-      1, 0, 0,
-      0, 1, 0,
-      x, y, 1,
-    ]),
-
-
-    scaling: (x, y) => ([
-      x, 0, 0,
-      0, y, 0,
-      0, 0, 1,
-    ]),
-
-    multiply: (a, b) => {
-      const factorValues = {
-        [FIRST]: [],
-        [SECOND]: [],
-        [THIRD]: []
-      }
-      for(let i = 0; i < 3; i++) {
-        factorValues[FIRST].push(a[i * 3 + 0])
-        factorValues[SECOND].push(a[i * 3 + 1])
-        factorValues[THIRD].push(a[i * 3 + 2])
-      }
-    
-      const [firstX, firstY, firstZ] = factorValues[FIRST]
-      const [secondX, secondY, secondZ] = factorValues[SECOND]
-      const [thirdX, thirdY, thirdZ] = factorValues[THIRD]
-      const multipliedMatrix = []
-      for (let i = 0; i < 3; i++) {
-        const [x, y, z] = [b[i * 3 + 0], b[i * 3 + 1], b[i * 3 + 2]]
-        multipliedMatrix.push(
-          x * firstX + y * firstY + z * firstZ,
-          x * secondX + y * secondY + z * secondZ,
-          x * thirdX + y * thirdY + z * thirdZ,
-        )
-      }
-      return multipliedMatrix
-    },
-
-    translate: (matrix, x, y) => that.multiply(matrix, that.transition(x, y)),
-
-    rotate: (matrix, angleInRadians) => that.multiply(matrix, that.rotation(angleInRadians)),
-
-    scale: (matrix, x, y) => that.multiply(matrix, that.scaling(x, y)),
-  }
-  return that
-}())
-
-class WebGl {
+class WebGL {
   constructor({ canvasId, vertexShaderText, fragmetShaderText }) {
     this.canvas = document.getElementById(canvasId)
     this.context = this.canvas.getContext('webgl')
@@ -136,89 +61,6 @@ class WebGl {
     this[bufferName] = context.createBuffer()
     context.bindBuffer(context.ARRAY_BUFFER, this[bufferName])
   
-    return this
-  }
-
-  drawF(props) {
-    const { x, y, translateX, translateY, angleInRadians, scaleX, scaleY } = props
-    var width = 100;
-    var height = 150;
-    var thickness = 30;
-    
-    const letterCoordinates = [
-       // вертикальный столб
-       x, y,
-       x + thickness, y,
-       x, y + height,
-       x, y + height,
-       x + thickness, y,
-       x + thickness, y + height,
-
-       // верхняя перекладина
-       x + thickness, y,
-       x + width, y,
-       x + thickness, y + thickness,
-       x + thickness, y + thickness,
-       x + width, y,
-       x + width, y + thickness,
-
-       // перекладина посередине
-       x + thickness, y + thickness * 2,
-       x + width * 2 / 3, y + thickness * 2,
-       x + thickness, y + thickness * 3,
-       x + thickness, y + thickness * 3,
-       x + width * 2 / 3, y + thickness * 2,
-       x + width * 2 / 3, y + thickness * 3,
-    ]
-
-    const vertiecesQuantity = letterCoordinates.length / 2
-    // let colors = []
-    // while (colors.length < vertiecesQuantity * 3) {
-    //   colors.push(145, 86, 255)
-    // }
-    // colors = colors.slice(0, vertiecesQuantity * 3)
-
-    const { clientWidth, clientHeight } = this.canvas 
-    const projectionMatrix = m3.projection(clientWidth, clientHeight)
-    let matrix
-    matrix = m3.scale(projectionMatrix, scaleX, scaleY)
-    matrix = m3.translate(matrix, translateX, translateY)
-    matrix = m3.rotate(matrix, angleInRadians)
-
-    this.setPositions({
-      coordinates: letterCoordinates,
-    })
-      .enableAttribute({
-        attributeName: 'a_position',
-        attributeElementsQuantity: 2,
-      })
-      // .setUniformResolution()
-      // .setUniform('u_transition', '2f', translateX, translateY)
-      // .setUniform('u_rotation', '2f', rotateX , rotateY)
-      // .setUniform('u_scale', '2f', scaleX, scaleY)
-      .setUniform('u_matrix', 'Matrix3fv', false, matrix)
-      .setUniform('u_color', '4f', 0.3, 0.5, 1, 1)
-      // .setColors({
-      //   colors,
-      // })
-      // .enableAttribute({
-      //   attributeName: 'a_color',
-      //   attributeElementsQuantity: 3,
-      //   type: 'UNSIGNED_BYTE',
-      //   isNormalized: true,
-      // })
-      .clearCanvas()
-      .drawVertieces(vertiecesQuantity)
-  }
-
-  setColors(props) {
-    this.createBuffer('colorsBuffer')
-  
-    const { colors, drawType = 'STATIC_DRAW' } = props
-    const { context } = this
-
-    context.bufferData(context.ARRAY_BUFFER, new Uint8Array(colors), context[drawType])    
-
     return this
   }
 
@@ -363,6 +205,65 @@ class WebGl {
   }
 }
 
+const m3 = Util.m3
+
+class WebGl2D extends WebGL {
+
+  drawF(props) {
+    const { x, y, translateX, translateY, angleInRadians, scaleX, scaleY } = props
+    var width = 100;
+    var height = 150;
+    var thickness = 30;
+    
+    const letterCoordinates = [
+       // вертикальный столб
+       x, y,
+       x + thickness, y,
+       x, y + height,
+       x, y + height,
+       x + thickness, y,
+       x + thickness, y + height,
+
+       // верхняя перекладина
+       x + thickness, y,
+       x + width, y,
+       x + thickness, y + thickness,
+       x + thickness, y + thickness,
+       x + width, y,
+       x + width, y + thickness,
+
+       // перекладина посередине
+       x + thickness, y + thickness * 2,
+       x + width * 2 / 3, y + thickness * 2,
+       x + thickness, y + thickness * 3,
+       x + thickness, y + thickness * 3,
+       x + width * 2 / 3, y + thickness * 2,
+       x + width * 2 / 3, y + thickness * 3,
+    ]
+
+    const vertiecesQuantity = letterCoordinates.length / 2
+
+    const { clientWidth, clientHeight } = this.canvas 
+    const projectionMatrix = m3.projection(clientWidth, clientHeight)
+    let matrix
+    matrix = m3.scale(projectionMatrix, scaleX, scaleY)
+    matrix = m3.rotate(matrix, angleInRadians)
+    matrix = m3.translate(matrix, translateX, translateY)
+
+    this.setPositions({
+      coordinates: letterCoordinates,
+    })
+      .enableAttribute({
+        attributeName: 'a_position',
+        attributeElementsQuantity: 2,
+      })
+      .setUniform('u_matrix', 'Matrix3fv', false, matrix)
+      .setUniform('u_color', '4f', 0.3, 0.5, 1, 1)
+      .clearCanvas()
+      .drawVertieces(vertiecesQuantity)
+  }
+}
+
 function getEl(selector) {
   return document.querySelector(selector)
 }
@@ -387,15 +288,14 @@ function createRangeInput(props) {
 }
 
 function startWebGl (fragmetShaderText, vertexShaderText) {
-  const gl = new WebGl({ canvasId: 'gl', vertexShaderText, fragmetShaderText })
-  const { canvas } = gl
-  const defaultX = 50
+  const gl = new WebGl2D({ canvasId: 'gl', vertexShaderText, fragmetShaderText })
+  const { clientHeight, clientWidth } = gl.canvas
   let [translateX, translateY] = [0, 0]
   let angleInRadians = 0
   let [scaleX, scaleY] = [1, 1]
   const drawF = () => gl.drawF({
-    x: defaultX, 
-    y: defaultX,
+    x: 0, 
+    y: 0,
     translateX,
     translateY,
     angleInRadians,
@@ -409,8 +309,8 @@ function startWebGl (fragmetShaderText, vertexShaderText) {
   const inputs = [
     {
       name: 'translateX',
-      max: canvas.width,
-      min: -canvas.width,
+      max: clientWidth,
+      min: -clientWidth,
       $container: $rangeInputs,
       onChange: (value) => {
         translateX = value
@@ -419,8 +319,8 @@ function startWebGl (fragmetShaderText, vertexShaderText) {
     },
     {
       name: 'translateY',
-      max: canvas.height,
-      min: -canvas.height,
+      max: clientHeight,
+      min: -clientHeight,
       $container: $rangeInputs,
       onChange: (value) => {
         translateY = value
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,8 @@
+const FIRST = 'first'
+const SECOND = 'second'
+const THIRD = 'third'
+const FOURTH = 'fourth'
+
 class Util {
   static domShaderSrc(url) {
     const request = new XMLHttpRequest(url)
@@ -12,4 +17,166 @@ class Util {
       request.send()
     })
   }
+
+  static m3 = (function() {
+    const that = { 
+      projection: (width, height) => ([
+        2 / width, 0, 0,
+        0, -2 / height, 0,
+        -1, 1, 1,
+      ]),
+
+      identity: () => ([
+        1, 0, 0,
+        0, 1, 0,
+        0, 0, 1,
+      ]),
+
+      rotation: (angleInRadians) => {
+        const s = Math.sin(angleInRadians)
+        const c = Math.cos(angleInRadians)
+        return [
+          c, -s, 0,
+          s, c, 0,
+          0, 0, 1,
+        ]
+      },
+
+      transition: (x, y) => ([
+        1, 0, 0,
+        0, 1, 0,
+        x, y, 1,
+      ]),
+
+      scaling: (x, y) => ([
+        x, 0, 0,
+        0, y, 0,
+        0, 0, 1,
+      ]),
+
+      multiply: (a, b) => {
+        const [firstX, firstY, firstZ] = get2DMatrixRow(a, 0)
+        const [secondX, secondY, secondZ] = get2DMatrixRow(a, 1)
+        const [thirdX, thirdY, thirdZ] = get2DMatrixRow(a, 2)
+        const multipliedMatrix = []
+        for (let i = 0; i < 3; i++) {
+          const [x, y, z] = get2DMatrixRow(b, i)
+          multipliedMatrix.push(
+            x * firstX + y * firstY + z * firstZ,
+            x * secondX + y * secondY + z * secondZ,
+            x * thirdX + y * thirdY + z * thirdZ,
+          )
+        }
+        return multipliedMatrix
+      },
+
+      translate: (matrix, x, y) => that.multiply(matrix, that.transition(x, y)),
+
+      rotate: (matrix, angleInRadians) => that.multiply(matrix, that.rotation(angleInRadians)),
+
+      scale: (matrix, x, y) => that.multiply(matrix, that.scaling(x, y)),
+    }
+    return that
+  }())
+
+  static m4 = (function() {
+    const that = { 
+      projection: (width, height, depth) => ([
+        2 / width, 0, 0, 1,
+        0, -2 / height, 0, 1,
+        0, 0, 2 / depth, 1,
+        -1, 1, 1, 1,
+      ]),
+
+      identity: () => ([
+        1, 0, 0, 1,
+        0, 1, 0, 1,
+        0, 0, 1, 1,
+        1, 1, 1, 1,
+      ]),
+
+      rotationX: (angleInRadians) => {
+        const s = Math.sin(angleInRadians)
+        const c = Math.cos(angleInRadians)
+        return [
+          1, 0, 0, 0,
+          0, c, s, 0,
+          0, -s, c, 0,
+          0, 0, 0, 1,
+        ]
+      },
+
+      rotationY: (angleInRadians) => {
+        const s = Math.sin(angleInRadians)
+        const c = Math.cos(angleInRadians)
+        return [
+          c, 0, -s, 0,
+          0, 1, 0, 0,
+          s, 0, c, 0,
+          0, 0, 0, 1,
+        ]
+      },
+
+      rotationZ: (angleInRadians) => {
+        const s = Math.sin(angleInRadians)
+        const c = Math.cos(angleInRadians)
+        return [
+          c, s, 0, 0,
+          -s, c, 0, 0,
+          0, 0, 1, 0,
+          0, 0, 0, 1,
+        ]
+      },
+
+      transition: (x, y, z) => ([
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        x, y, z, 1,
+      ]),
+
+      scaling: (x, y, z) => ([
+        x, 0, 0, 0,
+        0, y, 0, 0, 
+        0, 0, z, 0,
+        0, 0, 0, 1,
+      ]),
+
+      multiply: (a, b) => {
+        const [firstX, firstY, firstZ, firstW] = get3DMatrixRow(a, 0)
+        const [secondX, secondY, secondZ, secondW] = get3DMatrixRow(a, 1)
+        const [thirdX, thirdY, thirdZ, thirdW] = get3DMatrixRow(a, 2)
+        const [fourthX, fourthY, fourthZ, fourthW] = get3DMatrixRow(a, 3)
+        const multipliedMatrix = []
+        for (let i = 0; i < 4; i++) {
+          const [x, y, z, w] = get3DMatrixRow(b, i)
+          multipliedMatrix.push(
+            x * firstX + y * firstY + z * firstZ + w * firstW,
+            x * secondX + y * secondY + z * secondZ + w * secondW,
+            x * thirdX + y * thirdY + z * thirdZ + w * thirdW,
+            x * fourthX + y * fourthY + z * fourthZ + w * fourthW,
+          )
+        }
+        return multipliedMatrix
+      },
+
+      translate: (matrix, x, y, z) => that.multiply(matrix, that.transition(x, y, z)),
+
+      scale: (matrix, x, y, z) => that.multiply(matrix, that.scaling(x, y, z)),
+
+      rotateX: (matrix, angleInRadians) => that.multiply(matrix, that.rotationX(angleInRadians)),
+
+      rotateY: (matrix, angleInRadians) => that.multiply(matrix, that.rotationY(angleInRadians)),
+
+      rotateZ: (matrix, angleInRadians) => that.multiply(matrix, that.rotationZ(angleInRadians)),
+    }
+  }())
+}
+
+function get2DMatrixRow(matrix, row) {
+  return [matrix[row * 3 + 0], matrix[row * 3 + 1], matrix[row * 3 + 2]]
+}
+
+function get3DMatrixRow(matrix, row) {
+  return [matrix[row * 4 + 0], matrix[row * 4 + 1], matrix[row * 4 + 2], matrix[row * 4 + 3]]
 }
